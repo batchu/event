85e2f0708484d1140f3a3dc290e8b4d77484e9bb
pbatchu <pbatchu@DJX09CS1.CO.IHC.COM>
2013-09-17 17:11:38 -0600

    added add/list views for events,submissions,schema. WIP

--- HTML5Application/public_html/app/js/routes.js
+++ HTML5Application/public_html/app/js/routes.js
@@ -1,7 +1,12 @@
 event.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/events/list', {templateUrl: 'partials/events/list.html'});
+    $routeProvider.when('/schema/list', {templateUrl: 'partials/schema/list.html',controller:"eventCtrl"});
+    $routeProvider.when('/schema/add', {templateUrl: 'partials/schema/add.html',controller:"eventCtrl"});
+    $routeProvider.when('/events/list', {templateUrl: 'partials/events/list.html',controller:"eventCtrl"});
+    $routeProvider.when('/events/add', {templateUrl: 'partials/events/add.html',controller:"eventCtrl"});
+    $routeProvider.when('/submissions/list', {templateUrl: 'partials/submissions/list.html',controller:"eventCtrl"});
+    $routeProvider.when('/submissions/add', {templateUrl: 'partials/submissions/add.html',controller:"eventCtrl"});
 //    $routeProvider.when('/bills/add', {templateUrl: 'partials/bills/add.html', controller: 'BillAddCtrl'});
-    $routeProvider.otherwise({redirectTo: '/events/list'});
+    $routeProvider.otherwise({redirectTo: '/schema/list'});
   }]);
    
   event
