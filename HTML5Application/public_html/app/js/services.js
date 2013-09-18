102ee1928ee2f718c85c1a58226b4bf0614f0271
pbatchu <pbatchu@DJX09CS1.CO.IHC.COM>
2013-09-18 10:42:22 -0600

    reorganization in progress to work with the schemaList directly

--- HTML5Application/public_html/app/js/services.js
+++ HTML5Application/public_html/app/js/services.js
@@ -1,9 +1,85 @@
 event.factory('eventService',function(){
    var factory = {};
    
-var schemaList=[[{"type":"text","name":"Name","placeholder":"John Doe","order":10},{"name":"Name","type":"text","placeholder":"Event Name"},{"order":0,"name":"Packages","type":"select","options":[{"order":0,"name":"Starter","value":"s"},{"order":0,"name":"Intermediate","value":"i"},{"order":0,"name":"Advanced","value":"a"}]},{"order":0,"name":"Are you Single?","type":"radio","options":[{"order":0,"name":"Yes","value":"yes"},{"order":0,"name":"No","value":"no"}]},{"order":0,"type":"phone","name":"Phone Number"},{"order":0,"name":"Email","type":"email"},{"order":0,"type":"text","name":"Special Instructions"}]];
+var schemaList=[
+  [{
+      name:"evSchema1",
+      id:null,
+      schema:[
+    {
+      "type": "text",
+      "name": "Name",
+      "placeholder": "John Doe",
+      "order": 10
+    },
+    {
+      "name": "Name",
+      "type": "text",
+      "placeholder": "Event Name"
+    },
+    {
+      "order": 0,
+      "name": "Packages",
+      "type": "select",
+      "options": [
+        {
+          "order": 0,
+          "name": "Starter",
+          "value": "s"
+        },
+        {
+          "order": 0,
+          "name": "Intermediate",
+          "value": "i"
+        },
+        {
+          "order": 0,
+          "name": "Advanced",
+          "value": "a"
+        }
+      ]
+    },
+    {
+      "order": 0,
+      "name": "Are you Single?",
+      "type": "radio",
+      "options": [
+        {
+          "order": 0,
+          "name": "Yes",
+          "value": "yes"
+        },
+        {
+          "order": 0,
+          "name": "No",
+          "value": "no"
+        }
+      ]
+    },
+    {
+      "order": 0,
+      "type": "phone",
+      "name": "Phone Number"
+    },
+    {
+      "order": 0,
+      "name": "Email",
+      "type": "email"
+    },
+    {
+      "order": 0,
+      "type": "text",
+      "name": "Special Instructions"
+    }
+  ]
+  }],
+  []
+];
 
+var schemaIndex = 0;
+var newFieldIndex=0;
 var submissions=[];
+
  factory.getSchemaList=function(){
      return schemaList;
  };
@@ -15,6 +91,22 @@ var submissions=[];
      return submissions;
  };
  
+ 
+  factory.getSchemaIndex=function(){
+     return schemaIndex;
+ };
+ factory.setschemaIndex=function(_schemaIndex){
+    this.schemaIndex=_schemaIndex;
+ };
+ 
+  factory.getNewFieldIndex=function(){
+     return newFieldIndex;
+ };
+ factory.setNewFieldIndex=function(_newFieldIndex){
+    this.newFieldIndex=_newFieldIndex;
+ };
  return factory;
+ 
+ 
 });
 
